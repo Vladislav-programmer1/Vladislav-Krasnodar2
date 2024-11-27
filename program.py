f9d9471e5b7247d5544ffb220cc51ba39a142ee0
Shchegolev <shegolevva2009@gmail.com>
2024-11-27 17:49:43 +0300

    Программа со случайным кругом

--- program.py
+++ program.py
@@ -41,6 +41,7 @@ class CircleDrawing(QWidget):
 def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
 
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     form = CircleDrawing()
